55befb5b10004c6bf0da05687b63687cdfa8abdf
yuvi595 <jinshang267@gmail.com>
2024-11-20 17:01:19 +0530

    Added Storyline routes and templates for dark fantasy theme

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,16 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html', title="AdventureQuest Builder")
+
+@main.route('/start')
+def start():
+    return render_template('start.html', title="Start Your Journey")
+
+
+@main.route('/castle')
+def castle():
+    return render_template('castle.html', title="Haunted Castle")
+
+@main.route('/forest')
+def forest():
+    return render_template('forest.html', title="Dark Forest")
